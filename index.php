15fa0901cce80bd3931395144f31248a9d96f223
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-08-06 22:06:23 +0100

    a

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
 
     <?php
         $branch_name = shell_exec('git branch --show-current');
-        $branch_name = shell_exec('git');
+        $branch_name = shell_exec('git --version');
         $hash = shell_exec('sudo git rev-parse --short HEAD');
         $commit_msg = shell_exec('git log -1 --pretty=format:%B');
         $last_updated_time = shell_exec('git log -1 --format=%cd');
