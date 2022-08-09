9370bb7413a4b09a7557b25844a847698b53923d
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-08-09 13:43:47 +0100

    test

---
+++ commandline.php
@@ -0,0 +1,6 @@
+<?php
+    echo '<form action="commandexec.php" method="post" style="display:center; text-align:center;">';
+    echo 'What to execute? <input name="command"></input>';
+    echo '<p><button type="submit" class="header_button" onclick="" style="text-align:flex;">EXECUTE</button></p>';
+    echo '</form>';
+?>
