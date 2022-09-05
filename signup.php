f118150c9aaec067dd86dace0e0d39841ef60bfa
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-09-06 00:17:03 +0100

    typo

--- signup.php
+++ signup.php
@@ -21,7 +21,7 @@
             ?>
 
             <?php
-                if(!isset($_COOKIE['user']) or $_COOKIE['user'] == null or $_COOKIE['user'] == ""){
+                if(!isset($_COOKIE['name']) or $_COOKIE['name'] == null or $_COOKIE['name'] == ""){
                     echo '<p class="narrator" style="font-size: medium; text-align: center; ">信息录入</p>';
                     echo '<p class="narrator" style="font-size: medium; text-align: center; ">在下面输入昵称(登录用户名)，邮箱号，备注。已注册用户自动发送新密码到邮箱，未注册用户会先发邮件给管理员验证通过后再得到新密码，如果一直没收到，请检查垃圾邮件。</p>';
                     echo '<form action="signform.php" method="post" style="display:center;">
@@ -36,7 +36,7 @@
                     echo '<img src="./logo_2022.png" id="logo" alt="Weicheng_Space_Welcome_Message" style=" text-align: left; border-radius:20px; display:inline-block; height:100px; width:auto;">
                     <img src="./logo.png" id="logo" alt="Weicheng_Space_Welcome_Message" style=" text-align: left; border-radius:20px; display:inline-block; height:100px; width:auto;">';
 
-                    echo '<p class="narrator" style="font-size: medium; text-align: center; "><strong>'.$_COOKIE['user'].'</strong>，你已经登录了，如需得到新密码，请在下方确认信息重置。</p>';
+                    echo '<p class="narrator" style="font-size: medium; text-align: center; "><strong>'.$_COOKIE['name'].'</strong>，你已经登录了，如需得到新密码，请在下方确认信息重置。</p>';
                     echo '<form action="signform.php" method="post" style="display:center;">
                         <p>登录用户名[必填]: <input name="name" class="input_font" value="'.$_COOKIE['name'].'"></input></p>
                         <p>邮箱(用于重置密码)/(你的注册邮箱)[必填]: <input name="email" class="input_font" value="'.$_COOKIE['password'].'"></input></p>
