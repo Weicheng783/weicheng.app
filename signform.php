42c44aad753c18ea62aa3a1ba52beab29db72cfe
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-09-05 02:44:10 +0100

    fix

--- signform.php
+++ signform.php
@@ -95,7 +95,7 @@
                             // echo '<p class="narrator" style="font-size: medium; text-align: center; ">这是今日第 '.$row_count.' 次请求, 共5次。</p>';
 
                             // SENDING LOGIC
-                            $strstr = "echo \"TA正在尝试注册用户名：".$_COOKIE['name'].", 邮箱号为：".$_COOKIE['email'].", 想和你说的话是：".$_COOKIE['note'].", TA是今日尝试注册。产生点字符串来庆祝一下：".random_str(50)." <weicheng.app注册请求审核系统>\" | mail -s \"weicheng.app注册请求审核\" weicheng.ao@student.manchester.ac.uk";
+                            $strstr = "echo \"TA正在尝试注册用户名：".$_REQUEST['name'].", 邮箱号为：".$_REQUEST['email'].", 想和你说的话是：".$_REQUEST['note'].", TA是今日尝试注册。产生点字符串来庆祝一下：".random_str(50)." <weicheng.app注册请求审核系统>\" | mail -s \"weicheng.app注册请求审核\" weicheng.ao@student.manchester.ac.uk";
                             $result_str = shell_exec($strstr);
 
                             $pdo->query("INSERT INTO `request` (`name`, `email`, `password`, `ip`) VALUES ('".random_str(10)."', '".random_str(10)."', '".random_str(10)."', '".$_SERVER['REMOTE_ADDR']."')");
@@ -108,25 +108,25 @@
 
                         if($_COOKIE['email'] === $rows[0]['email']){
                             echo '<p class="narrator" style="font-size: medium; text-align: center; ">用户名、邮箱号与已知记录一致，正在检查是否超过每日重发次数，每日每用户最多可重试5次</p>';
-                            $re = $pdo->query("SELECT * FROM request WHERE name='".$_SERVER['name']."';");
-                            $row_count = $stmt->rowCount();
+                            // $re = $pdo->query("SELECT * FROM request WHERE name='".$_SERVER['name']."';");
+                            // $row_count = $stmt->rowCount();
     
-                            if($row_count >= 5){
-                                echo '<p class="narrator" style="font-size: medium; text-align: center; ">❌请求数量过多，请明天再试。</p>';
-                            }else{
-                                $row_count = $row_count + 1;
-                                echo '<p class="narrator" style="font-size: medium; text-align: center; ">这是今日第 '.$row_count.' 次请求, 共5次。</p>';
+                            // if($row_count >= 5){
+                            //     echo '<p class="narrator" style="font-size: medium; text-align: center; ">❌请求数量过多，请明天再试。</p>';
+                            // }else{
+                            //     $row_count = $row_count + 1;
+                            //     echo '<p class="narrator" style="font-size: medium; text-align: center; ">这是今日第 '.$row_count.' 次请求, 共5次。</p>';
     
                                 // SENDING LOGIC
                                 $genpwd = random_str(12);
-                                $strstr = "echo \"TA正在尝试更改用户名：".$_COOKIE['name'].", 邮箱号为：".$_COOKIE['email'].", 想和你说的话是：".$_COOKIE['note'].", TA是今日第 ".$row_count." 次更改。TA的新密码是：".$genpwd." <weicheng.app注册请求审核系统>\" | mail -s \"weicheng.app用户管理系统\" weicheng.ao@student.manchester.ac.uk";
-                                $strstr = "echo \"你正在尝试更改用户名：".$_COOKIE['name'].", 邮箱号为：".$_COOKIE['email'].", 想和管理员说的话是：".$_COOKIE['note'].", 你是今日第 ".$row_count." 次更改。你的新密码是：".$genpwd." <weicheng.app用户端邮件已送达>\" | mail -s \"weicheng.app用户端邮件\" ".$_COOKIE['email']."";
+                                $strstr = "echo \"TA正在尝试更改用户名：".$_REQUEST['name'].", 邮箱号为：".$_REQUEST['email'].", 想和你说的话是：".$_REQUEST['note'].", TA的新密码是：".$genpwd." <weicheng.app注册请求审核系统>\" | mail -s \"weicheng.app用户管理系统\" weicheng.ao@student.manchester.ac.uk";
+                                $strstr = "echo \"你正在尝试更改用户名：".$_REQUEST['name'].", 邮箱号为：".$_REQUEST['email'].", 想和管理员说的话是：".$_REQUEST['note'].", 你的新密码是：".$genpwd." <weicheng.app用户端邮件已送达>\" | mail -s \"weicheng.app用户端邮件\" ".$_COOKIE['email']."";
                                 $result_str = shell_exec($strstr);
     
-                                // $pdo->query("UPDATE `user` (`name`, `email`, `password`, `ip`) VALUES ('".random_str(10)."', '".random_str(10)."', '".random_str(10)."', '".$_SERVER['REMOTE_ADDR']."')");
+                                $pdo->query("UPDATE `user` SET `password`='".$genpwd."' WHERE `name`='".$_REQUEST['name']."';");
                                 echo '<p class="narrator" style="font-size: medium; text-align: center; ">✅成功！你的密码理论上已经更改，新密码已发至你的邮箱，注意查收，时常检查垃圾邮箱的收信情况，你现在可以退出了。</p>';
     // UPDATE `alternations` SET `time`='".date('Y/m/d H:i:s', time())."' WHERE `diary_id` = ".$id.";
-                            }
+                            // }
 
                         }else{
                             echo '<p class="narrator" style="font-size: medium; text-align: center; color: red;">❌用户名、邮箱号与已知记录不一致，你是不是输错了什么字符？请返回重试。</p>'; 
