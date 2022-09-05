59e6abb3c64ca2111528f4084b172218a0d0ce85
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-09-05 14:51:01 +0100

    a

--- approval.php
+++ approval.php
@@ -27,7 +27,7 @@
                     $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
                     // $re = $pdo->$query("SELECT * FROM request WHERE name='".htmlspecialchars($_GET['email'])."' AND password='".htmlspecialchars($_GET['password'])."';");
-                    $re = $pdo->$query("SELECT * FROM request;");
+                    $re = $pdo->$query("SELECT * FROM request");
                     $rows = $re->fetchAll();
                     if($rows == NULL){
                         echo '<p class="narrator" style="font-size: medium; text-align: center; ">❌查无此信息，不予放行。</p>';
