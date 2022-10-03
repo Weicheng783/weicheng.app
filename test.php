a5eb91555693bfa49374c2f3fcd12e4b239266c1
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-10-03 22:30:43 +0100

    push

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
 $mail_pwd = "&*12345*&";
 
-$result_str_r = shell_exec("sudo useradd -m -p $(openssl passwd -1 ".$mail_pwd.") testb");
+$result_str_r = shell_exec("sudo useradd -m -p $(openssl passwd -1 ".$mail_pwd.") testb 2>&1");
 ?>
